e168af5ff7a5e69df097de4fe7667f067eeb5f97
RexAbhishek <abhishekrajamar@gmail.com>
2025-04-15 14:04:12 +0530

    new changes

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+// app/fonts.ts
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'], 
+});
